e84815f075fb2c202afeb1ed85af96506858a9a8
Philipp Schuler <irgusite@gmail.com>
2014-01-27 22:56:04 +0100

    added stargate entity

--- src/Ftm/blogBundle/Controller/BlogController.php
+++ src/Ftm/blogBundle/Controller/BlogController.php
@@ -21,4 +21,34 @@ class BlogController extends Controller
 	
 	return $this->render("FtmblogBundle:Blog:messages.html.twig", $messages);
   }
+  public function addStargateAction()
+  {
+		$validation=false;
+		
+		$player = new Inscription;
+		$formBuilder = $this->createFormBuilder($player);
+		
+		$formBuilder
+				->add('location',        'text')
+				->add('adress',      'text');
+		$form = $formBuilder->getForm();
+
+		$request = $this->get('request');
+	
+		if ($request->getMethod() == 'POST') {
+		 
+		  $form->bind($request);
+		  
+		  if ($form->isValid()) {
+			
+			$repo = $this->getDoctrine()->getManager();
+			$repo->persist($player);
+			$repo->flush();
+			$validation = true;
+			return $this->render('FtmblogBundle:Page:stargate.html.twig', array('valid'=>$validation, 'form' => $form->createView(),));
+		  }
+		}
+		
+        return $this->render('FtmPlayerBundle:Default:form.html.twig', array('valid'=>$validation, 'form' => $form->createView(),));
+  }
 }
---
+++ src/Ftm/blogBundle/Entity/stargate.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Ftm\blogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * stargate
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Ftm\blogBundle\Entity\stargateRepository")
+ */
+class stargate
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="location", type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adress", type="string", length=10)
+     */
+    private $adress;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set location
+     *
+     * @param string $location
+     * @return stargate
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * Get location
+     *
+     * @return string 
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * Set adress
+     *
+     * @param string $adress
+     * @return stargate
+     */
+    public function setAdress($adress)
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    /**
+     * Get adress
+     *
+     * @return string 
+     */
+    public function getAdress()
+    {
+        return $this->adress;
+    }
+}
